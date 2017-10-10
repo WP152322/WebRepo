66bcccf977acd76bf2075d814938114567796ae1
suyeon <tnduswhddls@naver.com>
2017-10-10 15:25:31 +0900

    0

---
+++ WebClass/WebContent/js/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+	$('#loginForm').submit(function() {
+		// 자동 submit되는 기능을 막음
+		event.preventDefault();
+
+		// id, pwd값을 가져오기
+		// document.getElementById("id").value
+		var id = $('#id').val();
+		var pwd = $('#pwd').val();
+
+		// 서버로 post 방식으로 전송
+		$.post("http://httpbin.org/post", {
+			id : id,
+			pwd : pwd
+		}, function(data) {
+			// alert(data.form.id + "님 로그인되었습니다.");
+			var myModal = $('#myModal');
+			myModal.modal();
+			myModal.find('.modal-body').text(data.form.id + "님 로그인되었습니다.");
+		});
+	});
+});
+
+$(document).ready(
+		function() {
+			$('#joinus').submit(
+					function() {
+						event.preventDefault();
+						var name = $('#name').val();
+
+						$.post("http://httpbin.org/post", {
+							name : name
+						}, function(data) {
+							var myModal3 = $('#myModal3');
+							myModal3.modal();
+							myModal3.find('.modal-body').text(
+									data.form.name + "님 회원가입이 완료되었습니다");
+							var myModal2 = $('#myModal2');
+							myModal2.modal('hide');
+						});
+					});
+		});
+
+function myModal2show() {
+	var modal2 = $('#myModal2');
+	modal2.modal();
+
+}
---
+++ WebClass/src/org/dimigo/servlet/SessionServlet.java
@@ -0,0 +1,58 @@
+package org.dimigo.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.dimigo.vo.UserVO;
+
+/**
+ * Servlet implementation class SessionServlet
+ */
+@WebServlet("/session")
+public class SessionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SessionServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 세션에서 사용자 정보가 들어 있지 않으면 (null)이면 login.jsp로 포워딩
+		// 사용자 정보가 들어있다면 sessionInfo.jsp로 포워딩
+		// sessionInfo.jsp에서는 id, name, nickname를 출력
+		
+		
+		if(request.getSession().getAttribute("user") == null){
+				response.sendRedirect("jsp/login.jsp");
+			} else{
+				RequestDispatcher rd = 
+						request.getRequestDispatcher("jsp/sessionInfo.jsp");
+				rd.forward(request, response);
+			}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
